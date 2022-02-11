efcd6cf79bd338fba9b864cd03b0cf4498fa2ea2
Daniel Bautista <dbautista9015@gmail.com>
2022-02-11 11:53:57 -0800

    Added Links to all comp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { About } from './components/About';
+import { Inspiration } from './components/Inspiration'
+import { Home } from './components/Home';
+import { BestWorks } from './components/BestWorks';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        {/* <ul className="nav justify-content-end">
+          <li className="nav-item">
+            <Link to="/" className="nav-link" aria-current="page">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="about" className="nav-link" aria-current="page">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="inspiration" className="nav-link" aria-current="page">Inspiration</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="bestWorks" className="nav-link" aria-current="page">Best Works</Link>
+          </li>
+        </ul> */}
+        
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/inspiration" element={<Inspiration />} />
+          <Route path="/bestWorks" element={<BestWorks />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
